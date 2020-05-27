67e6092592c699a10f2b272629e819f4719a2628
Pance Ribarski <smokimk@gmail.com>
2020-05-27 12:39:26 +0200

    update integration

--- init_users.sh
+++ init_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat kong-konga-keycloak/dump_kong.sql | docker exec -i integration-soa2020_kong-db_1 psql -U kong -d postgres
-cat kong-konga-keycloak/dump_keycloak.sql | docker exec -i integration-soa2020_keycloak-db_1 psql -U keycloak -d postgres
+cat ../kong-konga-keycloak/dump_kong.sql | docker exec -i integration-soa2020_kong-db_1 psql -U kong -d postgres
+cat ../kong-konga-keycloak/dump_keycloak.sql | docker exec -i integration-soa2020_keycloak-db_1 psql -U keycloak -d postgres
